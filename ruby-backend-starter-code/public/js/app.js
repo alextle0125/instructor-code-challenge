ff21cfe73672e9ffe2f8d28e99356fb52a48e5ad
Alex Le <alextle0125@gmail.com>
2015-06-03 00:45:18 -0400

    design elements

--- ruby-backend-starter-code/public/js/app.js
+++ ruby-backend-starter-code/public/js/app.js
@@ -5,7 +5,7 @@
     var searchList = document.querySelector('ul');
     var input = document.querySelector('input').value;
 
-    function requestSearchData() {
+    function retreiveSearchData() {
       var xhr = new XMLHttpRequest();
       xhr.open('get', 'http://omdbapi.com/?s=' + encodeURIComponent(input), true);
       xhr.addEventListener('load', function(response){
@@ -17,18 +17,22 @@
     function generateListData(res) {
       for(var i = 0; i < res.length; i++){
           var node = document.createElement('li');
-          node.innerText = res[i].Title;
+
+          var anchor = document.createElement('a');
+          anchor.innerText = res[i].Title;
+          node.appendChild(anchor);
+
           searchList.appendChild(node);
         }
     }
-    
+
     if(searchList.children.length > 0){
       while (searchList.firstChild) {
         searchList.removeChild(searchList.firstChild)
       }
-      requestSearchData();
+      retreiveSearchData();
     } else {
-      requestSearchData();
+      retreiveSearchData();
     }
   });
 })();
