540586a695e3ed33c9f307f5327f437aeb0b6052
Alex Le <alextle0125@gmail.com>
2015-06-02 20:24:21 -0400

    init app

--- ruby-backend-starter-code/config/environment.rb
+++ ruby-backend-starter-code/config/environment.rb
@@ -35,7 +35,7 @@ configure do
 end
 
 # Set up the controllers and helpers
-Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| puts file }
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
